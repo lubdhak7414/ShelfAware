4589a99b36355839ca738262046a5e6a13267872
lubdhak7414 <jawadssubscription@gmail.com>
2023-03-21 20:15:00 +0600

    Add PDO connection and application configuration

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+// Application configuration — copy this file to config.local.php and adjust credentials.
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'shelfaware');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_CHARSET', 'utf8mb4');
+
+define('APP_NAME', 'ShelfAware');
+define('LOAN_DAYS', 14);
+define('FINE_PER_DAY', 0.50);
+
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
