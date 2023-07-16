2ca2329194bd87e3700d2f0ff472818d7f356f7c
lubdhak7414 <jawadssubscription@gmail.com>
2023-07-16 20:00:00 +0600

    Add staff activity log

---
+++ staff_admin.php
@@ -0,0 +1,121 @@
+<?php
+require_once __DIR__ . '/config.php';
+require_once __DIR__ . '/db.php';
+require_once __DIR__ . '/helpers.php';
+
+require_staff('admin');
+
+$pdo   = get_pdo();
+$msg   = '';
+$error = '';
+
+// Add new staff
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['add_staff'])) {
+    $username = trim($_POST['username'] ?? '');
+    $email    = trim($_POST['email'] ?? '');
+    $role     = $_POST['role'] ?? 'librarian';
+    $pass     = $_POST['password'] ?? '';
+
+    if ($username === '' || $email === '' || $pass === '') {
+        $error = 'All fields are required.';
+    } else {
+        $hash = md5($pass);
+        // Naive INSERT
+        $pdo->query("INSERT INTO staff (Username, Password, Email, Role) VALUES ('$username', '$hash', '$email', '$role')");
+
+        $staff_id = $_SESSION['staff_id'];
+        $pdo->query("INSERT INTO activity_log (Staff_id, Action, EntityType, EntityId, CreatedAt)
+                     VALUES ($staff_id, 'Added staff account: $username', 'staff', LAST_INSERT_ID(), NOW())");
+        $msg = 'Staff account created.';
+    }
+}
+
+// Delete staff
+if (isset($_GET['delete_staff']) && (int)$_GET['delete_staff'] !== (int)$_SESSION['staff_id']) {
+    $sid = (int)$_GET['delete_staff'];
+    $pdo->query("DELETE FROM staff WHERE Staff_id = $sid");
+    $staff_id = $_SESSION['staff_id'];
+    $pdo->query("INSERT INTO activity_log (Staff_id, Action, EntityType, EntityId, CreatedAt)
+                 VALUES ($staff_id, 'Deleted staff account', 'staff', $sid, NOW())");
+    redirect('/staff_admin.php?deleted=1');
+}
+
+$staff_list = $pdo->query("SELECT * FROM staff ORDER BY Username")->fetchAll();
+$logs       = $pdo->query("SELECT l.*, s.Username FROM activity_log l JOIN staff s ON l.Staff_id = s.Staff_id ORDER BY l.CreatedAt DESC LIMIT 50")->fetchAll();
+
+$pageTitle = 'Admin Panel — ' . APP_NAME;
+require __DIR__ . '/partials/header.php';
+?>
+
+<h2 class="mb-4">Admin Panel</h2>
+
+<?php if ($msg):   ?><div class="alert alert-success"><?= e($msg) ?></div><?php endif; ?>
+<?php if ($error): ?><div class="alert alert-danger"><?= e($error) ?></div><?php endif; ?>
+<?php if (!empty($_GET['deleted'])): ?><div class="alert alert-warning">Staff account deleted.</div><?php endif; ?>
+
+<div class="row g-4">
+    <div class="col-lg-5">
+        <h4>Staff Accounts</h4>
+        <table class="table table-sm table-hover">
+            <thead><tr><th>Username</th><th>Email</th><th>Role</th><th></th></tr></thead>
+            <tbody>
+            <?php foreach ($staff_list as $s): ?>
+            <tr>
+                <td><?= e($s['Username']) ?></td>
+                <td><?= e($s['Email']) ?></td>
+                <td><span class="badge bg-secondary"><?= e($s['Role']) ?></span></td>
+                <td>
+                    <?php if ($s['Staff_id'] != $_SESSION['staff_id']): ?>
+                    <a href="/staff_admin.php?delete_staff=<?= e($s['Staff_id']) ?>"
+                       class="btn btn-sm btn-outline-danger"
+                       onclick="return confirm('Remove this account?')">Remove</a>
+                    <?php endif; ?>
+                </td>
+            </tr>
+            <?php endforeach; ?>
+            </tbody>
+        </table>
+
+        <h5 class="mt-4">Add Staff Account</h5>
+        <form method="post">
+            <div class="mb-2">
+                <input type="text" class="form-control form-control-sm" name="username" placeholder="Username" required>
+            </div>
+            <div class="mb-2">
+                <input type="email" class="form-control form-control-sm" name="email" placeholder="Email" required>
+            </div>
+            <div class="mb-2">
+                <input type="password" class="form-control form-control-sm" name="password" placeholder="Password" required>
+            </div>
+            <div class="mb-2">
+                <select class="form-select form-select-sm" name="role">
+                    <option value="librarian">Librarian</option>
+                    <option value="admin">Admin</option>
+                </select>
+            </div>
+            <button type="submit" name="add_staff" class="btn btn-sm btn-success">Add Staff</button>
+        </form>
+    </div>
+
+    <div class="col-lg-7">
+        <h4>Recent Activity Log</h4>
+        <table class="table table-sm table-striped">
+            <thead><tr><th>Time</th><th>Staff</th><th>Action</th><th>Entity</th></tr></thead>
+            <tbody>
+            <?php if (empty($logs)): ?>
+            <tr><td colspan="4" class="text-muted">No activity yet.</td></tr>
+            <?php endif; ?>
+            <?php foreach ($logs as $l): ?>
+            <tr>
+                <td><small><?= e($l['CreatedAt']) ?></small></td>
+                <td><?= e($l['Username']) ?></td>
+                <td><?= e($l['Action']) ?></td>
+                <td><small><?= e($l['EntityType']) ?> #<?= e($l['EntityId']) ?></small></td>
+            </tr>
+            <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
+</div>
+
+<?php require __DIR__ . '/partials/footer.php'; ?>
